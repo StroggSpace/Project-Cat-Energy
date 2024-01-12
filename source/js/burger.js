b6e9734aed242413bd7de8c81852b9db501a05db
Juriy_Strogg <Strog1998@gmail.com>
2024-01-13 01:58:00 +0500

    Adaptive and retina img + other fix

--- source/js/burger.js
+++ source/js/burger.js
@@ -6,7 +6,11 @@ navMain.classList.add("site-nav--closed");
 navToggle.addEventListener("click", function () {
   if (navMain.classList.contains("site-nav--closed")) {
     navMain.classList.remove("site-nav--closed");
+    navToggle.classList.remove("toggle");
+    navToggle.classList.add("toggle--opened");
   } else {
     navMain.classList.add("site-nav--closed");
+    navToggle.classList.remove("toggle--opened");
+    navToggle.classList.add("toggle");
   }
 });
